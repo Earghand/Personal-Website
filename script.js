d06157d15754d976f3206e5d40dd66d8b0975c2f
Elias <elias@Eliass-MacBook-Pro.local>
2022-05-05 12:49:40 -0700

    added routing for meal.ly

---
+++ script.js
@@ -0,0 +1,6 @@
+if(document.getElementById("meal") != null) {
+    document.getElementById("meal").onclick = function () {
+        console.log("TEST")
+        location.href="meal.ly.html"
+    }
+}
