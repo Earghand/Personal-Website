2b141b54319f02aec993d6f1d53de8cda6d4f18c
Elias <elias@Eliass-MacBook-Pro.local>
2022-05-05 22:36:13 -0700

    finished teammate finder

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 if(document.getElementById("meal") != null) {
     document.getElementById("meal").onclick = function () {
-        console.log("TEST")
         location.href="meal.ly.html"
+    }
+}
+
+if(document.getElementById("team") != null) {
+    document.getElementById("team").onclick = function () {
+        location.href="teammate.html"
     }
 }
